1c95bee92930c7fab0b5b271b9b433d0cbe2da01
ArthurYdalgo <arthurc@alunos.utfpr.edu.br>
2019-10-10 19:10:13 -0300

    Alphabet.py/json e importação
    Criação do script de geração de alfabeto baseado nas diferentes listas de diferentes tipos de elementos presentes no alfabeto.

--- alphabet.py
+++ alphabet.py
@@ -2,7 +2,7 @@ import json
 
 digits_list = ['0','1','2','3','4','5','6','7','8','9']
 
-lowerCase_list = ['_','a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z']
+lowerCase_list = ['_',' ','a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z']
 upperCase_list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 letters_list = lowerCase_list+upperCase_list
 
@@ -10,7 +10,7 @@ mathElements_list = ['+','-','/','*','%','^']
 
 logicElements_list = ['<','>','=','!']
 
-separators_list = ['(',')','[',']','{','}','"','\'','.',';',',']
+separators_list = ['(',')','[',']','{','}','"','\'','.',';',',',':']
 
 commentTag = '#'
 
@@ -32,5 +32,8 @@ for element in logicElements_list:
 for element in separators_list:
     pypAlphabet[element] = "separator"
 
-with open('pyp_alphabet.json', 'w') as f:
-    json.dump(pypAlphabet, f)
+try:
+    with open('pyp_alphabet.json', 'w') as f:
+        json.dump(pypAlphabet, f)
+except:
+    print("Error while creating 'pyp_alphabet.py'.")
