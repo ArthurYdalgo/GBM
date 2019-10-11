153ddc468be901f71047070a240d12544b63de7b
ArthurYdalgo <arthurc@alunos.utfpr.edu.br>
2019-10-11 02:01:03 -0300

    Correção de alfabeto
    Faltou a letra n

--- alphabet.py
+++ alphabet.py
@@ -2,7 +2,7 @@ import json
 
 digits_list = ['0','1','2','3','4','5','6','7','8','9']
 
-lowerCase_list = ['_',' ','a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z']
+lowerCase_list = ['_',' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 upperCase_list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 letters_list = lowerCase_list+upperCase_list
 
