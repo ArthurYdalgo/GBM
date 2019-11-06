a98145dd5d80ded8fb3738670a6bbaca99823a8b
ArthurYdalgo <arthurc@alunos.utfpr.edu.br>
2019-11-06 01:16:35 -0300

    Syntatic Library

--- gbm_syntactic.py
+++ gbm_syntactic.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import sys #biblioteca para usar comandos de sistema
-from lark import Lark #biblioteca de bnf
+from parseGraph import *
 
-def variable_mapper(token_source_code):
-    
-    l = Lark('''start: WORD "," WORD "!"
-            %import common.WORD   // imports from terminal library
-            %ignore " "           // Disregard spaces in text
-         ''')
-
-
-
-#def variable_checker(token_source_code,variable_map):
+#Syntax parser
+def parser(token_code):
+    print("to do...")
+ 
