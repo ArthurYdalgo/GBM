a28dfa828a2e6ec1e06f1b1837ae1835513f7972
ArthurYdalgo <arthurc@alunos.utfpr.edu.br>
2019-11-06 11:47:24 -0300

    Graph Parser

--- parseGraph.py
+++ parseGraph.py
@@ -1,29 +1,15 @@
-from reserved import *
-import networkx as nx
-import matplotlib.pyplot as plt
+from parseBNF import *
 
-graphs = {}
+# if(nToken in base_code[cToken]):
+#     print("Current token: {0}".format(nToken))
+# else:
+#     print("Unexpect '{0}' in line {1}, after '{2}' (line {3}).".format(nToken,nTokenLine,cToken,cTokenLine))
 
-base_code = nx.DiGraph()
-variable_declaration = nx.DiGraph()
-code_instructions = nx.DiGraph()
 
-def connectTokenToTokens(Graph,originToken,endToken_):    
-    if (type(endToken_) is str):
-        Graph.add_edge(originToken,endToken_)
-            
-    elif(type(endToken_) is list ):
-        for token in endToken_:
-            Graph.add_edge(originToken,token)
-    return Graph
-
-#<base_code>
-base_code = connectTokenToTokens(base_code,"var","<variable_declaration>")
-base_code = connectTokenToTokens(base_code,"<variable_declaration>","begin")
-base_code = connectTokenToTokens(base_code,"begin","<code_instruction>")
-base_code = connectTokenToTokens(base_code,"<code_instruction>",";")
-base_code = connectTokenToTokens(base_code,";","end")
-
+plt.figure("<base_code>")
 nx.draw(base_code, with_labels=True, font_weight='bold')
 
-#plt.show()
+plt.figure("<variable_declaration>")
+nx.draw(variable_declaration, with_labels=True, font_weight='bold')
+
+plt.show()
