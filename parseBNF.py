602b63bea24ee1f358ac373ec01db02e1ac6ed86
ArthurYdalgo <arthurc@alunos.utfpr.edu.br>
2019-11-07 02:32:20 -0300

    Parse Graph Generator

--- parseBNF.py
+++ parseBNF.py
@@ -1,36 +1,66 @@
-from reserved import *
+# -*- coding: utf-8 -*-
+import sys #biblioteca para usar comandos de sistema
+from Reserved.reserved import *
+from Alphabet.alphabet import *
 import networkx as nx
 import matplotlib.pyplot as plt
 
+listOfGraphs=["base_code","variable_declaration"]
+
 graphs = {}
 
-base_code = nx.DiGraph()
-variable_declaration = nx.DiGraph()
-code_instructions = nx.DiGraph()
+def unTokenName(name):
+    return name[1:len(name)-1]
 
-def connectTokenToTokens(Graph,originToken,endToken_):    
+def connectTokenToTokens(Graph,originToken,endToken_):   
+    global graphs
     if (type(endToken_) is str):
-        Graph.add_edge(originToken,endToken_)
+        graphs[Graph].add_edge(originToken,endToken_)
             
     elif(type(endToken_) is list ):
         for token in endToken_:
-            Graph.add_edge(originToken,token)
-    return Graph
+            graphs[Graph].add_edge(originToken,token)
+    
+def generateParseGraph():
+    #<base_code>
+    global graphs
+    name = "<base_code>"
+    graphs[name] = nx.DiGraph()
 
-#<base_code>
-base_code = connectTokenToTokens(base_code,"var","begin")
-base_code = connectTokenToTokens(base_code,"var","<variable_declaration>")
-base_code = connectTokenToTokens(base_code,"<variable_declaration>","begin")
-base_code = connectTokenToTokens(base_code,"begin","<code_instruction>")
-base_code = connectTokenToTokens(base_code,"<code_instruction>","<code_instruction>")
-base_code = connectTokenToTokens(base_code,"<code_instruction>","end")
+    connectTokenToTokens(name,"var","begin")
+    connectTokenToTokens(name,"var","<variable_declaration>")
+    connectTokenToTokens(name,"<variable_declaration>","begin")
+    connectTokenToTokens(name,"begin","<code_instruction>")
+    connectTokenToTokens(name,"<code_instruction>","<code_instruction>")
+    connectTokenToTokens(name,"<code_instruction>","end")
 
-#<variable_declaration>
-variable_declaration = connectTokenToTokens(variable_declaration,"<variable_declaration>",data_type_list)
-for dataType in data_type_list:
-    variable_declaration = connectTokenToTokens(variable_declaration,dataType,"<id_token>")
-variable_declaration = connectTokenToTokens(variable_declaration,"<id_token>",",")
-variable_declaration = connectTokenToTokens(variable_declaration,"<id_token>",";")
-variable_declaration = connectTokenToTokens(variable_declaration,",","<id_token>")
+
+    #<variable_declaration>
+    name = "<variable_declaration>"
+    graphs[name] = nx.DiGraph()
+
+
+    connectTokenToTokens(name,"<variable_declaration>",data_type_list)
+    for dataType in data_type_list:
+        connectTokenToTokens(name,dataType,"<id_token>")
+    connectTokenToTokens(name,"<id_token>",",")
+    connectTokenToTokens(name,"<id_token>",";")
+    connectTokenToTokens(name,",","<id_token>")
+
+    
+
+#exportação dos grafos de derivação
+def exportGraph():
+    global graphs
+    for graph in graphs:        
+        nx.write_gml(graphs[graph], "parseGraphs/{0}.gml".format(unTokenName(graph)))
+
+try:
+    if(sys.argv[1]=="run"):    
+        generateParseGraph()        
+        exportGraph()
+except:        
+    pass
+
 
 
