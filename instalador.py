60bad5ab77944246fedb3e6f49c82c9f2d0ca347
ArthurYdalgo <arthurc@alunos.utfpr.edu.br>
2019-12-01 14:07:41 -0300

    Libraries Installer

--- instalador.py
+++ instalador.py
@@ -2,8 +2,9 @@ import os
 
 os.command("pip install networkx")
 os.command("pip install inspect")
-#os.command("pip install prettytable")
-os.command("python parseBNF.py run")
-os.command("python Reserved/reserved_generator.py")
-os.command("python Alphabet/alphabet_generator.py")
+os.command("pip install prettytable")
+os.command("pip install Pillow")
+#os.command("python parseBNF.py run")
+#os.command("python Reserved/reserved_generator.py")
+#os.command("python Alphabet/alphabet_generator.py")
 
